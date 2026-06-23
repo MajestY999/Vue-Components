a48b21b7ea6ee24521707127a2f421c0b84c3256
MajestY999 <Vilisovartem@gmail.com>
2026-06-23 20:58:08 +1000

    Задание 1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" style="text-align: center; padding-top: 40px; font-family: sans-serif; color: green;">
+    <h1>Как работают слоты  в вью</h1>
+
+
+    <BaseAlert>
+      Это первое уведомление, здесь просто текст. 
+    </BaseAlert>
+
+    
+    <BaseAlert>
+      <b>Внимание!</b> Это очень важное жирное сообщение. 
+    </BaseAlert>
+
+    
+    <BaseAlert>
+      Что-то пошло не так. Возможно есть ошибка <a href="#">здесь</a>.
+    </BaseAlert>
+  </div>
+</template>
+
+<script setup>
+import BaseAlert from './components/BaseAlert.vue'
+</script>
